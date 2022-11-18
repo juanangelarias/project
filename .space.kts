9bab500bb7434c9015bc26fd2fa0ebd865258bf0
Juan Arias <juanangelarias@gmail.com>
2022-11-18 00:12:51 +0000

    A

--- .space.kts
+++ .space.kts
@@ -4,6 +4,11 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("Build and publish") {
+    container(displayName = "Build and notify", image = "gradle"){
+        kotlinScript { api ->
+            api.gradlew("build")
+            api.space().projects.planing.issues.createIssue()
+        }
+    }
 }
